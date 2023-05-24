30be81d8993665d7bbb117238be30c277cddd53a
jenkins <builds@apache.org>
2023-05-24 16:18:56 +0000

    Updated asf-site from main at 33893d0aa8c258a5d6889fbd05a176404986867a
    Built from https://ci-builds.apache.org/job/Community%20Development/job/events-site/job/main/79/

--- content/js/events-calendar.js
+++ content/js/events-calendar.js
@@ -17,7 +17,7 @@ if (!events) {
 
 // Options
 var now = (new Date()).toISOString();
-var opts = "?key=" + API_KEY 
+var opts = "?key=" + API_KEY
          + "&timeMin=" + now
          + "&singleEvents=true"
          + "&orderBy=startTime"
@@ -34,7 +34,7 @@ $.ajax({
        var el = response['items'];
        let reg = /T.*$/;
        let official = /(ApacheCon|Roadshow)/;
-       if (el.length > 0) { 
+       if (el.length > 0) {
           for (i = 0; i < el.length; i++) {
              var ev = el[i];
              var when = ev.start.dateTime;
