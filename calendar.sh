921e937c09aa1a93348468ccba2e8c3f222cec87
Sebb <sebb@apache.org>
2024-04-19 00:19:01 +0100

    Try fetching calendar

---
+++ calendar.sh
@@ -0,0 +1,7 @@
+CALENDAR_ID="nerseigospses068jd57bk5ar8@group.calendar.google.com"
+API_KEY="AIzaSyDJkXq1faq2G5NkFkFTh9Sikdpc2YXTVXs"
+NOW=$(date '+%Y-%m-%dT%H:%M:%SZ')
+OPTS="?key=${API_KEY}&timeMin=${NOW}&singleEvents=true&orderBy=startTime&timezone=UTC&maxResults=20"
+curl -sS -o calendar.json "https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events${OPTS}" \
+  -H 'accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'origin: https://events.apache.org'
