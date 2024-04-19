144841d7013f3c98773e22cfe8c451e2da1590d3
Sebb <sebbASF@users.noreply.github.com>
2024-04-19 09:33:38 +0100

    Update getcalendar.sh

--- getcalendar.sh
+++ getcalendar.sh
@@ -7,6 +7,12 @@ API_KEY="AIzaSyDJkXq1faq2G5NkFkFTh9Sikdpc2YXTVXs"
 NOW=$(date '+%Y-%m-%dT%H:%M:%SZ')
 OPTS="?key=${API_KEY}&timeMin=${NOW}&singleEvents=true&orderBy=startTime&timezone=UTC&maxResults=20"
 
+echo "Fetching calendar data"
+ls -l calendar.json
+
 curl -sS -o calendar.json "https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events${OPTS}" \
   -H 'accept: application/json, text/javascript, */*; q=0.01' \
   -H 'origin: https://events.apache.org'
+
+echo "Fetched data"
+ls -l calendar.json
