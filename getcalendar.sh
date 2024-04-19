8ed607a355dba92bbc58bda2d0540f040c9a83e3
Sebb <sebb@apache.org>
2024-04-19 11:01:25 +0100

    Drop debug

--- getcalendar.sh
+++ getcalendar.sh
@@ -8,11 +8,9 @@ NOW=$(date '+%Y-%m-%dT%H:%M:%SZ')
 OPTS="?key=${API_KEY}&timeMin=${NOW}&singleEvents=true&orderBy=startTime&timezone=UTC&maxResults=20"
 
 echo "Fetching calendar data"
-ls -l calendar.json
 
 curl -sS -o calendar.json "https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events${OPTS}" \
   -H 'accept: application/json, text/javascript, */*; q=0.01' \
   -H 'origin: https://events.apache.org'
 
 echo "Fetched data"
-ls -l calendar.json
