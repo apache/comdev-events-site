b6cd41d421347f9959a01e976562445413c4fe77
Bertrand Delacretaz <bdelacretaz@apache.org>
2025-02-27 13:02:26 +0100

    Rename calendar.json to prevent unwanted edits

--- getcalendar.sh
+++ getcalendar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Fetch calendar data; will be stored as calendar.json
+# Fetch calendar data; will be stored as calendar-DO-NOT-EDIT.json
 
 CALENDAR_ID="nerseigospses068jd57bk5ar8@group.calendar.google.com"
 API_KEY="AIzaSyDJkXq1faq2G5NkFkFTh9Sikdpc2YXTVXs"
@@ -9,7 +9,7 @@ OPTS="?key=${API_KEY}&timeMin=${NOW}&singleEvents=true&orderBy=startTime&timezon
 
 echo "Fetching calendar data"
 
-curl -sS -o calendar.json "https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events${OPTS}" \
+curl -sS -o calendar-DO-NOT-EDIT.json "https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events${OPTS}" \
   -H 'accept: application/json, text/javascript, */*; q=0.01' \
   -H 'origin: https://events.apache.org'
 
