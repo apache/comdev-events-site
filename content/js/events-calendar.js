a0201c282d12479ca00f741b931a8f85e03fb96a
jenkins <builds@apache.org>
2025-09-16 16:22:06 +0000

    Updated asf-site from main at e4c3e05a7cdfa0959f6ef16b869c659e3d95636b
    Built from https://ci-builds.apache.org/job/Community%20Development/job/events-site/job/main/175/

--- content/js/events-calendar.js
+++ content/js/events-calendar.js
@@ -53,7 +53,10 @@ $.ajax({
                  html += ev.summary;
              }
              if (link) { html += "</a>"; }
-             html += " - <i>" + when + "</i>";
+             html += " - <a href='" + ev.htmlLink + "'><i>" + when + "</i></a>";
+             if (ev.description && !link) { // add description if not already used
+               html += "<br/>" + ev.description;
+             }
              if (ev.location) {
                 html += "<br/>"+ev.location;
              }
